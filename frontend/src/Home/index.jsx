521394be27bd250bc68d68d81ec2bbf1bce5b257
Sejal Jaiswal <710sejal@gmail.com>
2023-03-05 11:24:43 +0530

    add particles

---
+++ frontend/src/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss'
+
+export default function Home() {
+  return (
+    <div>
+        <div className='home'>
+           <div className='home__firstChild'> 
+
+           </div>
+        </div>
+    </div>
+  )
+}
