844d6099d5411be078c964ca6916f8e945c8e177
Sejal Jaiswal <710sejal@gmail.com>
2023-03-06 15:55:09 +0530

    ..

---
+++ frontend/src/start/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Particle from './Particel';
+
+export default function Start() {
+  return (
+    <div>
+        {
+            <Particle />
+        }
+        <button> Start </button>
+    </div>
+  )
+}
